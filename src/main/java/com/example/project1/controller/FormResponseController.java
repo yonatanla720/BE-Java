acc21525fef0083ac638af5b5e4da0c1bcbd6cbd
yonatanla720 <yon998@gmail.com>
2023-07-23 06:35:59 +0300

    fix cross origin

--- src/main/java/com/example/project1/controller/FormResponseController.java
+++ src/main/java/com/example/project1/controller/FormResponseController.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/form-response")
+@CrossOrigin(origins = "*")
 public class FormResponseController {
 
     private final FormResponseRepository formResponseRepository;
